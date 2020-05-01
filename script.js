54c5ea44d63a0963bcb77724863c194a4cf381ec
vd89 <me@daxcodes.in>
2020-05-01 15:54:28 +0530

    added the location for the city and working on the chart

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-let cityName = document.getElementById('cityName')
-let submit = document.getElementById('submit')
+let cityName = document.getElementById('cityName');
+let submit = document.getElementById('submit');
+let ui = new UI();
+let temp = document.getElementById('temp')
 
+let myChart = document.getElementById('myChart').getContext('2d')
+let lineChart = new Chart(myChart, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    }
+})
 
-// let myChart = document.getElementById('myChart').getContext('2d')
-// let lineChart = new Chart(myChart, {
-//     type: 'bar',
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [0, 10, 5, 2, 20, 30, 45]
-//         }]
-//     }
-// })
-
-
-
-function findCity(e){
-    e.preventDefault();
-    let cName = cityName.value    
-    getForecast(cName)
-    .then((res) => {
-        const { list, city } = res.data
-        console.log(city);
-    }).catch((err) => {
-        throw err
+function findCity(e) {
+  e.preventDefault();
+  let cName = cityName.value;
+  getForecast(cName)
+      .then((res) => {
+        const { list, city } = res.data;
+        ui.showDetails(city);       
+    })
+    .catch((err) => {
+        ui.showAlert('City not found', 'alert alert-danger')
+        ui.cityDetails()
     });
-    cityName.innerHTML =""
+  cityName.innerHTML = '';
 }
-// Event Listener 
-submit.addEventListener('submit',findCity)
+// Event Listener
+submit.addEventListener('submit', findCity);
--- ui.js
+++ ui.js
@@ -1,5 +1,42 @@
 class UI {
-    constructor() {
-        this.cityDetails = document.getElementById('cityDetails')
+  constructor() {
+    this.cityDetail = document.getElementById('cityDetail');
+  }
+
+  showDetails(city) {
+    this.cityDetail.innerHTML = `
+        <div class="card card-body mb-3">
+        <div class=" card-title"> <h4>${city.name}</h4></div>
+        <div class="card-body">
+        <h4 class="card-title"> Country: ${city.country} </h4>    
+        <p class="card-text">Population: ${city.population} </p>
+        <p class="card-text">Location: Lat: ${city.coord.lat}, Lon: ${city.coord.lon} </p>
+        </div>
+  </div>
+        `;
+  }
+  showAlert(message, className) {
+    this.clearAlert();
+    const div = document.createElement('div');
+    div.className = className;
+
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector('.searchContainer');
+    const search = document.querySelector('.search');
+    container.insertBefore(div, search);
+
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if (currentAlert) {
+      currentAlert.remove();
     }
+  }
+  clearCityName() {
+    this.cityDetail.innerHTML = '';
+  }
 }
